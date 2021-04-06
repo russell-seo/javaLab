9f93f6a794668accddcdbcdfd76e545e4888e623
kiaofk <kiaofk@naver.com>
2021-04-06 22:56:47 +0900

    람다식 확인문제 연습

---
+++ chap14Lab/src/check/LambdaExample.java
@@ -0,0 +1,35 @@
+package check;
+
+import java.util.function.IntBinaryOperator;
+
+public class LambdaExample {
+	private static int[] scores = { 10, 50, 3 };
+	public static int maxOrMin(IntBinaryOperator operator) {
+	int result = scores[0];
+	for(int score : scores) {
+	result = operator.applyAsInt(result, score);
+	}
+	return result;
+	}
+	public static void main(String[] args) {
+	//최대값 얻기
+		int max = maxOrMin( 
+		(a,b) ->{
+		if(a>=b) {
+			return a;
+		}else {
+			return b;
+	);
+		System.out.println("최대값: " + max);
+			int min = maxOrMin(
+			(a,b) ->{
+				if(a<=b) {
+					return b; 
+				}else return a;
+			}
+			);
+			System.out.println("최소값: " + min);
+			}
+		}
+	//최소값 얻기
+}
---
+++ chap14Lab/src/check/LamdaExample1.java
@@ -0,0 +1,39 @@
+package check;
+
+public class LamdaExample1 {
+	private static Student[] students = {
+			new Student("홍길동", 90, 96),
+			new Student("신용권", 95, 93)
+			};
+			 // avg() 메소드 작성 위치, #1
+			public static double avg(Tointfunction<Student> function) {
+				
+				for(Student student : students) {
+					
+				}
+			}
+
+	
+	
+			public static void main(String[] args) {
+			double englishAvg = avg( s -> s.getEnglishScore() );
+			System.out.println("영어 평균 점수: " + englishAvg);
+			double mathAvg = avg( s -> s.getMathScore() );
+			System.out.println("수학 평균 점수: " + mathAvg);
+			}
+			
+			
+			public static class Student {
+				private String name;
+				private int englishScore;
+				private int mathScore;
+			public Student(String name, int englishScore, int mathScore) {
+				this.name = name;
+				this.englishScore = englishScore;
+				this.mathScore = mathScore;
+			}
+			public String getName() { return name; }
+			public int getEnglishScore() { return englishScore; }
+			public int getMathScore() { return mathScore; }
+			}
+}
